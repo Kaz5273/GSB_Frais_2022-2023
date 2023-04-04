d8d42b31887cc152ec91e047df9e59b279587fc6
allard.kazuki <kazutor73@gmail.com>
2023-04-04 11:59:50 +0200

    ajout d'une liste de fichefrais et creation d'une fichefrais et lignefraishorsforfait

--- src/Controller/CurrentFicheFraisController.php
+++ src/Controller/CurrentFicheFraisController.php
@@ -8,8 +8,10 @@ use App\Entity\FraisForfait;
 use App\Entity\LigneFraisForfait;
 use App\Entity\LigneFraisHorsForfait;
 use App\Entity\User;
+use App\Form\ChoiceMoisType;
 use App\Form\LignesFraisHorsForfaitType;
 use App\Form\LignesFraisForfaitType;
+use App\Repository\LigneFraisHorsForfaitRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,6 +21,38 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CurrentFicheFraisController extends AbstractController
 {
+    #[Route('/fichefraisMois', name: 'app_fiche_frais_ListMois', methods: ['GET', 'POST'])]
+    public function afficheFrais(ManagerRegistry $doctrine, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user= $this->getUser();
+        $repository = $doctrine->getRepository(FicheFrais::class);
+        $fichesfrais = $repository->findBy(['user' => $user]);
+
+        foreach ($fichesfrais as $uneFicheFrais){
+            $listMois[] = $uneFicheFrais->getMois();
+        }
+        $form = $this->createForm(ChoiceMoisType::class, $listMois, []);
+        $form->handleRequest($request);
+        $bool = false;
+        $myFicheFrais = [];
+        if ($form->isSubmitted() && $form->isValid()) {
+            $bool = true;
+
+            $mois = $form->get('mois')->getData();
+            $myFicheFrais = $repository->findOneBy(['mois' => $mois, 'user' => $user]);
+
+        }
+
+        return $this->render('fiche_frais/index.html.twig', [
+            'fichefrais' => $myFicheFrais,
+            'listMois' => $listMois,
+            'form' => $form->createView(),
+            'bool' => $bool
+
+        ]);
+
+    }
+
     #[Route('/saisie', name: 'app_saisie_fiche_frais')]
     public function saisie(Request $request, EntityManagerInterface $entityManager, ManagerRegistry $doctrine): Response
     {
@@ -33,23 +67,32 @@ class CurrentFicheFraisController extends AbstractController
         $user= $this->getUser();
         $repositoryFiche = $doctrine->getRepository(FicheFrais::class);
         $mois = $moisEnCours->format('Ym');
-        $ficheFraisEnCours = $repositoryFiche->findOneBy(['mois' => $mois, 'user' => $user]);
+        $fichefrais = $repositoryFiche->findOneBy(['mois' => $mois, 'user' => $user]);
+        $repoFrais = $doctrine->getRepository(FraisForfait::class);
+        $fraisEtape = $repoFrais->find(1);
+        $fraisKilometrique = $repoFrais->find(2);
+        $fraisNuitee = $repoFrais->find(3);
+        $fraisRestaurant = $repoFrais->find(4);
 
         //Saisie FraisForfaitise
-        if($ficheFraisEnCours == null){
+        if($fichefrais == null){
             $fichefrais = new FicheFrais();
 
             $ligneFraisEtape = new LigneFraisForfait();
             $ligneFraisEtape->setQuantite(0);
+            $ligneFraisEtape->setFraisForfait($fraisEtape);
 
             $ligneFraisKilometrique = new LigneFraisForfait();
             $ligneFraisKilometrique->setQuantite(0);
+            $ligneFraisKilometrique->setFraisForfait($fraisKilometrique);
 
             $ligneFraisNuitee = new LigneFraisForfait();
             $ligneFraisNuitee->setQuantite(0);
+            $ligneFraisNuitee->setFraisForfait($fraisNuitee);
 
             $ligneFraisRestaurant = new LigneFraisForfait();
             $ligneFraisRestaurant->setQuantite(0);
+            $ligneFraisRestaurant->setFraisForfait($fraisRestaurant);
 
             $fichefrais->setUser($user);
             $fichefrais->setMois($mois);
@@ -66,49 +109,17 @@ class CurrentFicheFraisController extends AbstractController
             $entityManager->flush();
         }
 
-
-        $form2 = $this->createForm(LignesFraisForfaitType::class, $ficheFraisEnCours);
+        $form2 = $this->createForm(LignesFraisForfaitType::class, $fichefrais);
         $form2 ->handleRequest($request);
 
         if ($form2->isSubmitted() && $form2->isValid()) {
 
-            //Test si la fichefrais existe
-                $repository = $doctrine->getRepository(FraisForfait::class);
-
-                $fraisEtape = $repository->findOneBy(['id' => 1]);
-                $lignefraisForfaitEtape = new LigneFraisForfait();
-                $lignefraisForfaitEtape->setQuantite($form2->get('ForfaitEtape')->getData());
                 $fichefrais->getLigneFraisForfaits()[0]->setQuantite($form2->get('ForfaitEtape')->getData());
-                $lignefraisForfaitEtape->setFicheFrais($ficheFraisEnCours);
-                $lignefraisForfaitEtape->setFraisForfait($fraisEtape);
-
-                $fraisKilometrique = $repository->findOneBy(['id' => 2]);
-                $ligneFraisForfaitKilometrique = new LigneFraisForfait();
-                $ligneFraisForfaitKilometrique->setQuantite($form2->get('ForfaitKilometrique')->getData());
                 $fichefrais->getLigneFraisForfaits()[1]->setQuantite($form2->get('ForfaitKilometrique')->getData());
-                $ligneFraisForfaitKilometrique->setFicheFrais($ficheFraisEnCours);
-                $ligneFraisForfaitKilometrique->setFraisForfait($fraisKilometrique);
-
-                $fraisNuitee = $repository->findOneBy(['id' => 3]);
-                $ligneFraisForfaitNuitee = new LigneFraisForfait();
-                $ligneFraisForfaitNuitee->setQuantite($form2->get('ForfaitNuitee')->getData());
                 $fichefrais->getLigneFraisForfaits()[2]->setQuantite($form2->get('ForfaitNuitee')->getData());
-                $ligneFraisForfaitNuitee->setFicheFrais($ficheFraisEnCours);
-                $ligneFraisForfaitNuitee->setFraisForfait($fraisNuitee);
-
-                $fraisRestaurant = $repository->findOneBy(['id' => 4]);
-                $ligneFraisForfaitRestaurant = new LigneFraisForfait();
-                $ligneFraisForfaitRestaurant->setQuantite($form2->get('ForfaitRestaurant')->getData());
                 $fichefrais->getLigneFraisForfaits()[3]->setQuantite($form2->get('ForfaitRestaurant')->getData());
-                $ligneFraisForfaitRestaurant->setFicheFrais($ficheFraisEnCours);
-                $ligneFraisForfaitRestaurant->setFraisForfait($fraisRestaurant);
 
-
-            $entityManager->persist($lignefraisForfaitEtape);
-            $entityManager->persist($ligneFraisForfaitKilometrique);
-            $entityManager->persist($ligneFraisForfaitNuitee);
-            $entityManager->persist($ligneFraisForfaitRestaurant);
-            $entityManager->persist($ficheFraisEnCours);
+            $entityManager->persist($fichefrais);
             $entityManager->flush();
 
             return $this->redirectToRoute('app_fiche_frais_index', [], Response::HTTP_SEE_OTHER);
@@ -123,12 +134,12 @@ class CurrentFicheFraisController extends AbstractController
 
 
             //Saisie LigneFraisHorsForfait
-            $ligneFraisHorsForfait->setFicheFrais($ficheFraisEnCours);
+            $ligneFraisHorsForfait->setFicheFrais($fichefrais);
             $ligneFraisHorsForfait->setLibelle($form->get('libelle')->getData());
             $ligneFraisHorsForfait->setMontant($form->get('montant')->getData());
             $ligneFraisHorsForfait->setDate($form->get('date')->getData());
-            $ficheFraisEnCours->addLigneFraisHorsForfait($ligneFraisHorsForfait);
-            $entityManager->persist($ficheFraisEnCours);
+            $fichefrais->addLigneFraisHorsForfait($ligneFraisHorsForfait);
+            $entityManager->persist($fichefrais);
             $entityManager->persist($ligneFraisHorsForfait);
             $entityManager->flush();
 
@@ -138,10 +149,19 @@ class CurrentFicheFraisController extends AbstractController
         return $this->render('fiche_frais/new.html.twig', [
 
             'moisFrais' => $moisEnCours,
-            'fichesFrais' => $ficheFraisEnCours,
+            'fichesFrais' => $fichefrais,
             'form' => $form,
             'form2' => $form2
 
         ]);
+    }
+    #[Route('/lignefraishorsforfait/{id}', name: 'app_ligne_frais_hors_forfait_delete', methods: ['POST', 'GET'])]
+    public function delete(Request $request, LigneFraisHorsForfait $ligneFraisHorsForfait, LigneFraisHorsForfaitRepository $ligneFraisHorsForfaitRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$ligneFraisHorsForfait->getId(), $request->request->get('_token'))) {
+            $ligneFraisHorsForfaitRepository->remove($ligneFraisHorsForfait, true);
+        }
+
+        return $this->redirectToRoute('app_fiche_frais_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Controller/FicheFraisController.php
+++ src/Controller/FicheFraisController.php
@@ -8,6 +8,7 @@ use App\Entity\LigneFraisForfait;
 use App\Entity\LigneFraisHorsForfait;
 use App\Entity\User;
 use App\Form\ChoiceMoisType;
+use App\Form\FicheFraisType;
 use App\Form\LignesFraisHorsForfaitType;
 use App\Repository\FicheFraisRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -20,94 +21,69 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/fichefrais')]
 class FicheFraisController extends AbstractController
 {
-    #[Route('/', name: 'app_fiche_frais_index', methods: ['GET', 'POST'])]
-    public function afficheFrais(ManagerRegistry $doctrine, Request $request, EntityManagerInterface $entityManager): Response
+//    #[Route('/', name: 'app_fiche_frais_index', methods: ['GET', 'POST'])]
+//    public function afficheFrais(ManagerRegistry $doctrine, Request $request, EntityManagerInterface $entityManager): Response
+//    {
+//        $user= $this->getUser();
+//        $repository = $doctrine->getRepository(FicheFrais::class);
+//        $fichesfrais = $repository->findBy(['user' => $user]);
+//
+//        foreach ($fichesfrais as $uneFicheFrais){
+//            $listMois[] = $uneFicheFrais->getMois();
+//        }
+//        $form = $this->createForm(ChoiceMoisType::class, $listMois, []);
+//        $form->handleRequest($request);
+//        $bool = false;
+//        $myFicheFrais = [];
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $bool = true;
+//
+//            $mois = $form->get('mois')->getData();
+//            $myFicheFrais = $repository->findOneBy(['mois' => $mois, 'user' => $user]);
+//
+//        }
+//
+//        return $this->render('fiche_frais/index.html.twig', [
+//            'fichefrais' => $myFicheFrais,
+//            'listMois' => $listMois,
+//            'form' => $form->createView(),
+//            'bool' => $bool
+//
+//        ]);
+//
+//    }
+
+    #[Route('/', name: 'app_fiche_frais_index', methods: ['GET'])]
+    public function index(ManagerRegistry $doctrine): Response
     {
-        $user= $this->getUser();
+        $user = $this->getUser();
         $repository = $doctrine->getRepository(FicheFrais::class);
         $fichesfrais = $repository->findBy(['user' => $user]);
 
-        foreach ($fichesfrais as $uneFicheFrais){
-            $listMois[] = $uneFicheFrais->getMois();
-        }
-        $form = $this->createForm(ChoiceMoisType::class, $listMois, []);
-        $form->handleRequest($request);
-        $bool = false;
-        $myFicheFrais = [];
-        if ($form->isSubmitted() && $form->isValid()) {
-            $bool = true;
-
-            $mois = $form->get('mois')->getData();
-            $myFicheFrais = $repository->findOneBy(['mois' => $mois, 'user' => $user]);
-
-        }
-
-        return $this->render('fiche_frais/index.html.twig', [
-            'fichefrais' => $myFicheFrais,
-            'listMois' => $listMois,
-            'form' => $form->createView(),
-            'bool' => $bool
-
+        return $this->render('fiche_frais/listFicheFrais.html.twig', [
+            'fichesfrais' => $fichesfrais,
         ]);
-
     }
 
-    #[Route('/new', name: 'app_fiche_frais_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, ManagerRegistry $doctrine ): Response
+    #[Route('/new/', name: 'app_fiche_frais_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-
         $ficheFrai = new FicheFrais();
-        $form = $this->createForm(LignesFraisHorsForfaitType::class, $ficheFrai);
+        $form = $this->createForm(FicheFraisType::class, $ficheFrai);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
-            $repository = $doctrine->getRepository(FraisForfait::class);
-
-            $fraisEtape = $repository->findOneBy(['id' => 1]);
-            $lignefraisForfaitEtape = new LigneFraisForfait();
-            $lignefraisForfaitEtape->setQuantite($form->get('ForfaitEtape')->getData());
-            $lignefraisForfaitEtape->setFicheFrais($ficheFrai);
-            $lignefraisForfaitEtape->setFraisForfait($fraisEtape);
-
-            $fraisKilometrique = $repository->findOneBy(['id' => 2]);
-            $ligneFraisForfaitKilometrique = new LigneFraisForfait();
-            $ligneFraisForfaitKilometrique->setQuantite($form->get('ForfaitKilometrique')->getData());
-            $ligneFraisForfaitKilometrique->setFicheFrais($ficheFrai);
-            $ligneFraisForfaitKilometrique->setFraisForfait($fraisKilometrique);
-
-            $fraisNuitee = $repository->findOneBy(['id' => 3]);
-            $ligneFraisForfaitNuitee = new LigneFraisForfait();
-            $ligneFraisForfaitNuitee->setQuantite($form->get('ForfaitNuitee')->getData());
-            $ligneFraisForfaitNuitee->setFicheFrais($ficheFrai);
-            $ligneFraisForfaitNuitee->setFraisForfait($fraisNuitee);
-
-            $fraisRestaurant = $repository->findOneBy(['id' => 4]);
-            $ligneFraisForfaitRestaurant = new LigneFraisForfait();
-            $ligneFraisForfaitRestaurant->setQuantite($form->get('ForfaitRestaurant')->getData());
-            $ligneFraisForfaitRestaurant->setFicheFrais($ficheFrai);
-            $ligneFraisForfaitRestaurant->setFraisForfait($fraisRestaurant);
-
             $entityManager->persist($ficheFrai);
-            $entityManager->persist($lignefraisForfaitEtape);
-            $entityManager->persist($ligneFraisForfaitKilometrique);
-            $entityManager->persist($ligneFraisForfaitNuitee);
-            $entityManager->persist($ligneFraisForfaitRestaurant);
-
             $entityManager->flush();
 
             return $this->redirectToRoute('app_fiche_frais_index', [], Response::HTTP_SEE_OTHER);
-
         }
 
         return $this->renderForm('fiche_frais/new.html.twig', [
             'fiche_frai' => $ficheFrai,
             'form' => $form,
-
         ]);
     }
-
-
 
     #[Route('/{id}', name: 'app_fiche_frais_show', methods: ['GET'])]
     public function show(FicheFrais $ficheFrai): Response
@@ -120,7 +96,7 @@ class FicheFraisController extends AbstractController
     #[Route('/{id}/edit', name: 'app_fiche_frais_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, FicheFrais $ficheFrai, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(LignesFraisHorsForfaitType::class, $ficheFrai);
+        $form = $this->createForm(FicheFraisType::class, $ficheFrai);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -129,7 +105,7 @@ class FicheFraisController extends AbstractController
             return $this->redirectToRoute('app_fiche_frais_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('fiche_frais/edit.html.twig', [
+        return $this->render('fiche_frais/edit.html.twig', [
             'fiche_frai' => $ficheFrai,
             'form' => $form,
         ]);
