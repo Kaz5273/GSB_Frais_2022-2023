25da4e9dfb1e81117e4cdc5288f250e1d890cd0a
allard.kazuki <kazutor73@gmail.com>
2023-04-25 08:25:17 +0200

    Amelioration affichage de la liste de mois

--- src/Form/ChoiceMoisType.php
+++ src/Form/ChoiceMoisType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\FicheFrais;
+use IntlDateFormatter;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -19,8 +20,17 @@ class ChoiceMoisType extends AbstractType
                 'label' => 'Mois',
                 'mapped' => false,
                 'choices' => $ficheFrais,
-                'choice_label' =>function ($choices, $key, $value){
-                        return $value;
+                'choice_label' =>function ($choice){
+                    $dataObj = \DateTime::createFromFormat('Ym', $choice);
+                    $fmt = datefmt_create(
+                        'fr_FR',
+                        IntlDateFormatter::FULL,
+                        IntlDateFormatter::FULL,
+                        'Europe/Paris',
+                        IntlDateFormatter::GREGORIAN,
+                        'MMMM yyyy'
+                    );
+                        return datefmt_format($fmt, $dataObj);
                 },
                 'attr' => [
                     'placeholder' => 'Mois',
